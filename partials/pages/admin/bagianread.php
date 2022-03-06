5fc0a951ac9c59f7e2c0437c64757013a396cd94
Muhammad Fikri Anshari <FikriJoker45@gmail.com>
2022-03-06 22:38:51 +0800

    Update bagianread.php

--- partials/pages/admin/bagianread.php
+++ partials/pages/admin/bagianread.php
@@ -1,52 +1,96 @@
 <?php
-if (isset($_GET['id'])) {
+$database = new Database();
+$db = $database->getConnection();
 
-    $database = new Database();
-    $db = $database->getConnection();
+if (isset($_POST['button_create'])) {
+    $validateSql = "SELECT * FROM bagian WHERE nama_bagian = ?";
 
-    $id = $_GET['id'];
-    $findSql = "SELECT * FROM karyawan WHERE id = ?";
-    $stmt = $db->prepare($findSql);
-    $stmt->bindParam(1, $_GET['id']);
+    $stmt = $db->prepare($validateSql);
+    $stmt->bindParam(1, $_POST['nama_bagian']);
     $stmt->execute();
-    $row = $stmt->fetch();
-    if (isset($row['id'])) {
+    if ($stmt->rowCount() > 0) {
 ?>
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Karyawan</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="?page=home"> Home</li></a>
-                        <li class="breadcrumb-item"><a href="?page=karyawanread">Karyawan</li></a>
-                        <li class="breadcrumb-item active">Riwayat Bagian</li>
-                    </ol>
-                </div>
+        <div class="alert alert-danger alert-dismissible">
+            <button class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            <h5><i class="icon fas fa-ban"></i> Gagal</h5>
+            Nama bagian sama sudah ada
+        </div>
+<?php
+    } else {
+        $insertSql = "INSERT INTO bagian(id,nama_bagian,karyawan_id,lokasi_id) VALUES (null,?,?,?)";
+        $stmt = $db->prepare($insertSql);
+        $stmt->bindParam(1, $_POST['nama_bagian']);
+        $stmt->bindParam(2, $_POST['karyawan_id']);
+        $stmt->bindParam(3, $_POST['lokasi_id']);
+        if ($stmt->execute()) {
+            $_SESSION['hasil'] = true;
+            $_SESSION['pesan'] = "Berhasil simpan data";
+        } else {
+            $_SESSION['hasil'] = false;
+            $_SESSION['pesan'] = "Gagal simpan data";
+        }
+        echo '<meta http-equiv="refresh" content="0; url=?page=bagianread">';
+    }
+}
+?>
+
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Tambah Data Bagian</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="?page=home"> Home</li></a>
+                    <li class="breadcrumb-item"><a href="?page=bagianread"> Bagian</li></a>
+                    <li class="breadcrumb-item">Tambah Data</li>
+                </ol>
             </div>
         </div>
-    </section>
-    
-    <section class="content">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Riwayat Bagian</h3>
-            </div>
-            <div class="card-body">
-            <div class="row">
-                <div class="col-sm-6">
+    </div>
+</section>
+
+<section class="content">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Tambah Bagian</h3>
+        </div>
+        <div class="card-body">
+            <form method="post">
                 <div class="form-group">
-                    <label for="nik">Nomor Induk Karyawan</label>
-                    <input type="text" class="form-control" name="nik" value="<?php echo $row['nik'] ?>" disabled>
+                    <label for="nama_bagian">Nama Bagian</label>
+                    <input type="text" name="nama_bagian" class="form-control">
                 </div>
-                </div>
-                <div class="col-sm-6">
                 <div class="form-group">
-                    <label for="handphone">Handphone</label>
-                    <input type="text" class="form-control" name="handphone" value="<?php echo $row['handphone'] ?>" disabled>
+                    <label for="karyawan_id">Kepala Bagian</label>
+                    <select class="form-control select2bs4" name="karyawan_id" style="width: 100%;">
+                        <option value="">-- Pilih Kepala Bagian --</option>
+                        <?php
+                        $selectSql = "SELECT * FROM karyawan";
+                        $stmt_karyawan = $db->prepare($selectSql);
+                        $stmt_karyawan->execute();
+
+                        while ($row_karyawan = $stmt_karyawan->fetch(PDO::FETCH_ASSOC)) {
+                            echo '<option value="' . $row_karyawan["id"] . '">' . $row_karyawan["nama_lengkap"] . '</option>';
+                        }
+                        ?>
+                    </select>
                 </div>
+                <div class="form-group">
+                    <label for="karyawan_id">Lokasi Bagian</label>
+                    <select class="form-control select2bs4" name="lokasi_id" style="width: 100%;">
+                        <option value="">-- Pilih Lokasi Bagian --</option>
+                        <?php
+                        $selectSql = "SELECT * FROM lokasi";
+                        $stmt_lokasi = $db->prepare($selectSql);
+                        $stmt_lokasi->execute();
+
+                        while ($row_lokasi = $stmt_lokasi->fetch(PDO::FETCH_ASSOC)) {
+                            echo '<option value="' . $row_lokasi["id"] . '">' . $row_lokasi["nama_lokasi"] . '</option>';
+                        }
+                        ?>
+                    </select>
                 </div>
                 <a href="?page=lokasiread" class="btn btn-danger btn-sm float-right ml-2">
                     <i class="fa fa-times"></i> Batal
@@ -57,13 +101,12 @@ if (isset($_GET['id'])) {
             </form>
         </div>
     </div>
-            </div>
-        </div>
-    </section>
-<?php
-    } else {
-        echo "<meta http-equiv='refresh' content='0;url=?page=karyawanread'>";
-    }
-} else {
-    echo "<meta http-equiv='refresh' content='0;url=?page=karyawanread'>";
-}
+</section>
+
+<?php include_once "partials/scripts.php" ?>
+
+<script>
+    $('.select2bs4').select2({
+        theme: 'bootstrap4'
+    })
+</script>
