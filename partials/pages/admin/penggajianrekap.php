291c1310cc825f23a6e5390d68d58a6fbadd8a89
Muhammad Fikri Anshari <FikriJoker45@gmail.com>
2022-03-06 22:55:13 +0800

    Ekspor PDF Menggunakan Datatables

--- partials/pages/admin/penggajianrekap.php
+++ partials/pages/admin/penggajianrekap.php
@@ -113,5 +113,5 @@
             "autoWidth": false,
             "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
         }).buttons().container().appendTo('#mytable_wrapper .col-md-6:eq(0)');
-    });
+    }); //mudahan berhasil
 </script>
